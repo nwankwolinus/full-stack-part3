33c1b9b809ffbb671c9b12431a2b802915154ad3
nwankwolinus <51293362+nwankwolinus@users.noreply.github.com>
2023-07-22 03:57:26 +0100

    modified index

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
